6bc09b4a7236ba12e333264d116acc8beb5158a4
Ana Becker <anabdesigns@gmail.com>
2013-12-11 18:55:22 -0500

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ TwitterApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'tweetsandwiches#index'
 
   # See how all your routes lay out with "rake routes"
 
